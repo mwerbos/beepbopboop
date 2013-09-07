35f13cb680f6557904922d2603a6349d3bb067b6
maia <tigrennatenn@yahoo.com>
2013-09-07 15:40:14 -0400

    Updated rails version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Velociraptor::Application.config.secret_token = '65f366c3076f7d1a379295cb48116fcdfe7bc837aa7c7e58b3bcc4ace9ff052e9445467b229c1c17c38ce9d28dbeac2382ca16415e2c5f6266dfccee0ab677fb'
