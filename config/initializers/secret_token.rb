bb5504f658e4e214ea899b664ae494cb39f3878c
maia <tigrennatenn@yahoo.com>
2013-09-07 15:21:29 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Velociraptor::Application.config.secret_key_base = 'be91181bea1bac3e4add0cefd6fce0d39ce996cc1e0310dfa58ba4c365993494d7a54a7b73e46456b6ac7bd5440eab110b1633e711cc2b334dc2ce20d23292cd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Velociraptor::Application.config.session_store :cookie_store, key: '_velociraptor_session'
