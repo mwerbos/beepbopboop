05b335cd6659413e6cc1c98d1cf303bbdde1cb26
maia <tigrennatenn@yahoo.com>
2013-09-08 06:09:05 -0400

    Reordered browse pages

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -9,7 +9,7 @@ class ActivitiesController < ApplicationController
 
   def browse
     # TODO: make per_page settable?
-    @activities = Activity.paginate(:page => params[:page], :per_page => 10)
+    @activities = Activity.reorder("id DESC").paginate(:page => params[:page], :per_page => 10)
     respond_to do |format|
       format.html
     end
