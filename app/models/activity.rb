1e4ade7ab597f69e6a45e1de66c5f5b4ca03d8dd
Roger Curley <rocurley@gmail.com>
2013-09-08 06:32:48 -0400

    Made some default values for preferences, mostly.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,5 @@
 class Activity < ActiveRecord::Base
+  require File.join(Rails.root,"lib/choose_optimal.rb")
   has_many :preferences
   attr_accessible :name
   def generate_availability
@@ -7,12 +8,12 @@ class Activity < ActiveRecord::Base
     prefs.each do |pref|
       data.merge!({pref.user => pref.times}){|k,v1,v2| v1+v2}
     end
-	out = {}
-	out["users"] =data.keys.map do |user|
-      {id: user.id,username:user.username,times: data[user]}
-    end
-	out["times"] = []
-    out.to_json
+    out = {}
+    out["users"] =data.keys.map do |user|
+        {id: user.id,username:user.username,times: data[user]}
+      end
+    out["times"] = choose_optimal(self)
+      out.to_json
   end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
   belongs_to :activity
+  serialize  :users
   # attr_accessible :title, :body
 end
