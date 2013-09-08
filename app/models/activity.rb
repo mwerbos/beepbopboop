f5f2cde8e59be1da622424c798ad640d23551a54
Joseph Owen <josephowen92@gmail.com>
2013-09-08 06:06:15 -0400

    Workin' on visualization.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -7,9 +7,11 @@ class Activity < ActiveRecord::Base
     prefs.each do |pref|
       data.merge!({pref.user => pref.times}){|k,v1,v2| v1+v2}
     end
-    out=data.keys.map do |user|
+	out = {}
+	out["users"] =data.keys.map do |user|
       {id: user.id,username:user.username,times: data[user]}
     end
+	out["times"] = []
     out.to_json
   end
 
