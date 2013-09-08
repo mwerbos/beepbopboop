8fc839f9fc7c1a506c244c5c9d540b1b02dac85e
Joseph Owen <josephowen92@gmail.com>
2013-09-08 08:29:22 -0400

    changes to visualize.js

--- app/assets/javascripts/visualize.js
+++ app/assets/javascripts/visualize.js
@@ -8,32 +8,26 @@ function visualize() {
     if (window.gon.timeData == undefined)
         return;
         
-    var dataset = JSON.parse(window.gon.timeData)
+    // var dataset = JSON.parse(window.gon.timeData)
     
     border = 50
     
-    // var dataset = {
-        // "users": [
-            // {"id": 1, "times": [{"start":(new Date(2001,0,1)), "end":(new Date(2001, 0, 4))}]}, 
-            // {"id": 2, "times": [{"start":(new Date(2001,0,2)), "end":(new Date(2001, 0, 6))}]},
-            // {"id": 3, "times": [{"start":(new Date(2001,0,1)), "end":(new Date(2001, 0, 8))}]},
-            // {"id": 4, "times": [{"start":(new Date(2001,0,9)), "end":(new Date(2001, 0, 12))}]}
-        // ],
-    // "times": [{
-        // "start":(new Date(2001,0,2)),
-        // "end":(new Date(2001, 0, 4))
-        // }]};
-        
-    // console.log(dataset)
-    // console.log(dataset.users)
-    // console.log(dataset.users[0].id)
-    // console.log(dataset.users[0].times[0])
-    // console.log(dataset.users[0].times[0].start)
+    var dataset = {
+        "users": [
+            {"id": 1, "times": [{"start":(new Date(2001,0,1)), "end":(new Date(2001, 0, 4))}, {"start":(new Date(2001,0,7)), "end":(new Date(2001, 0, 10))}]}, 
+            {"id": 2, "times": [{"start":(new Date(2001,0,2)), "end":(new Date(2001, 0, 6))}]},
+            {"id": 3, "times": [{"start":(new Date(2001,0,1)), "end":(new Date(2001, 0, 8))}]},
+            {"id": 4, "times": [{"start":(new Date(2001,0,9)), "end":(new Date(2001, 0, 12))}]}
+        ],
+    "times": [{
+        "start":(new Date(2001,0,2)),
+        "end":(new Date(2001, 0, 4))
+        }]};
         
     var w = $(window).width() - 50;
-    var h = 400;
-    var w2 = w + border*2;
-    var h2 = h + border*2;
+    var h = 600;
+    var w2 = w - border*2;
+    var h2 = h - border*2;
 
     var minDate = new Date();
     var maxDate = new Date(minDate.getTime() + (1000*60*60*24*7));
@@ -56,13 +50,13 @@ function visualize() {
     var timesScale = d3.time.scale()
         // .domain([minDate, maxDate])
         .domain([minTime, maxTime])
-        .range([border,w2-border]);
+        .range([border,w-border]);
         
     console.log(timesScale(minTime))
 
     var svg = d3.select("#visualization").append("svg")
-        .attr("width", w2)
-        .attr("height", h2);
+        .attr("width", w)
+        .attr("height", h);
     
     var rects = svg.selectAll("rect")
         .data(dataset.users)
@@ -74,12 +68,12 @@ function visualize() {
         return timesScale(new Date(d.times[0].start));
     })
     .attr("y", function(d, i) {
-        return i*(h / dataset.users.length) + (h/dataset.users.length)/4 + border;
+        return i*(h2 / dataset.users.length) + (h2/dataset.users.length)/4 + border;
     })
     .attr("width", function(d) {
         return timesScale(new Date(d.times[0].end)) - timesScale(new Date(d.times[0].start))
     })
-    .attr("height", h / dataset.users.length * 1/2);
+    .attr("height", h2 / dataset.users.length * 1/2);
       
       
     var ranges = svg.selectAll()
@@ -97,16 +91,15 @@ function visualize() {
     .attr("width", function(d) {
         return timesScale(new Date(d.end)) - timesScale(new Date(d.start))
     })
-    .attr("height", h);
+    .attr("height", h2);
                 
     var xAxis = d3.svg.axis()
         .scale(timesScale)
         .orient("bottom")
-        .ticks(7)
         .tickSize(5);
             
     svg.append("g")
         .attr("class", "axis")
-        .attr("transform", "translate(0," + (h) + ")")
+        .attr("transform", "translate(0," + (h-border) + ")")
         .call(xAxis);
 }
