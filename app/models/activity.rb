33f3c1ebc4432648536a394f3e0404fa1d50a7fa
Roger Curley <rocurley@gmail.com>
2013-09-08 02:51:42 -0400

    Added a method to activitys for pretty pictures

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,16 @@
 class Activity < ActiveRecord::Base
+  has_many :preferences
   attr_accessible :name
+  def generate_availability
+    prefs=self.preferences
+    data={}
+    prefs.each do |pref|
+      data.merge!({pref.user => pref.times}){|k,v1,v2| v1+v2}
+    end
+    out=data.keys.map do |user|
+      {id: user.id,username:user.username,times: data[user]}
+    end
+    out.to_json
+  end
+
 end
